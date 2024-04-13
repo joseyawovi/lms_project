13180b599936f8956b1c9efd68cc0c184ca7e67a
joseyawovi <joseyawovi@gmail.com>
2024-04-13 23:21:29 +0530

    commitment

--- LMS/LMS/asgi.py
+++ LMS/LMS/asgi.py
@@ -14,4 +14,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LMS.settings')
 
 application = get_asgi_application()
-#
+#jsut to be able to commit
